a8b91dfe2de55195fee136df71211e20ea535e71
TeutaMemaj <95043686+TeutaMemaj@users.noreply.github.com>
2023-06-03 16:06:29 +0200

    src

---
+++ clientfrontend/src/data/Products.js
@@ -0,0 +1,70 @@
+const products = [
+  {
+    _id: "1",
+    name: "Honest Hydrogel Cream",
+    image: "/images/6.png",
+    description:
+      "The cooling, Hyaluronic Acid infused face cream your thirsty skin craves. Made with Squalane, Jojoba Esters, and 2 sizes of Hyaluronic Acid. Works for all skin types.",
+    price: 89,
+    countInStock: 60,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    _id: "2",
+    name: "Honey Starter Set",
+    image: "/images/5.png",
+    description:
+      "Honey is a natural humectant known to retain and preserve skin's moisture. This kit includes a Honey cleanser, Manuka Honey Day Cream, Manuka Honey Night Cream and Royal Jelly Eye Cream. This kit features Manuka Honey and Royal Jelly to moisturize, calm and sooth skin. Calming, Moisturizing, Soothing.",
+    price: 100,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    _id: "3",
+    name: "Ultra Dark Self Tan Mousse  By mine",
+    image: "/images/4.png",
+    description:
+      "Achieve ultra dark bronzed, glowing skin in as little as an hour with MineTan Ultra Dark Self Tan Mousse. Perfect for more experienced tanners and those wanting the darkest tan results. The Triple Action Formula of our 3 tan actives Erythrulose, DHA & Bronzers works to produce a more even, longer-lasting, streak-free finish whilst keeping the skin deeply and more consistently hydrated - all to develop the deepest, most natural looking tan attainable. Lightweight, fast drying and with a tropical coconut scent it’s your perfect partner to get that iconic beach bronze.",
+    price: 35,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    _id: "4",
+    name: "Ordinary moisturizing factors +HA",
+    image: "/images/3.png",
+    description:
+      "Natural Moisturizing Factors + HA is a moisturizer that works with your skin to support its natural hydration barrier. By using a collection of ingredients that appear naturally in your skin—such as amino acids, fatty acids, triglycerides, urea, ceramides, phospholipids, glycerine, saccharides, sodium PCA, and hyaluronic acid—this cream keeps the outer layer of your skin protected and well-hydrated, without feeling greasy. Plus, it provides both immediate and lasting effects, as it works to significantly improve skin barrier function with regular use.",
+    price: 20,
+    countInStock: 25,
+    rating: 5,
+    numReviews: 9,
+  },
+  {
+    _id: "5",
+    name: "Rituals smooth cleansing foam",
+    image: "/images/2.png",
+    description:
+      "A silky smooth cream that turns into a nourishing cleansing foam when you massage it onto wet skin. Added moringa and lotus will soothe and hydrate your complexion.",
+    price: 35,
+    countInStock: 50,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    _id: "6",
+    name: "Clay Cleanser By Nash | Jones",
+    image: "/images/1.png",
+    description:
+      "A very deep cleanser for acne prone and oily skin. Mineral-rich, it deep cleans pores of dirt, make-up and impurities. SKIN TYPE: For deep cleansing oily skin and acne prone skin. This formula has a high pH [9-10] so use it sparingly for a deeper clean as needed.",
+    price: 25,
+    countInStock: 100,
+    rating: 0,
+    numReviews: 0,
+  },
+];
+
+export default products;
